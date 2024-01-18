aacc046568fcd1d7caa80c50246c031864fc75a8
haseebahmed <haseebahmed02@gmail.com>
2024-01-18 23:22:50 +0000

    34: Generic Repository
    Added GenericRepository for ListAllAsync and GetByIdAsync Method

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+	//  Base Entity is used because we have inherited other entities from this
+	// Reason of creating BaseEntity was to use with Generics
+	public interface IGenericRepository<T> where T: BaseEntity
+	{
+		Task<T> GetByIdAsync(int id);
+
+		Task<IReadOnlyList<T>> ListAllAsync();
+	}
+}
+
