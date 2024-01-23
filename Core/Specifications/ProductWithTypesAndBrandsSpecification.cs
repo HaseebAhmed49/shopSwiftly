3836062dd6708d4f19b350429e6f8e6c7f8f695e
haseebahmed <haseebahmed02@gmail.com>
2024-01-23 22:28:05 +0000

    Added Comment Step 41 for Specification Pattern

--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -6,12 +6,14 @@ namespace Core.Specifications
 {
 	public class ProductWithTypesAndBrandsSpecification: BaseSpecification<Product>
 	{
+		// Constructor that is using the BaseSpecification No Parameter Constructor
 		public ProductWithTypesAndBrandsSpecification()
 		{
 			AddInclude(x => x.ProductType);
 			AddInclude(x => x.ProductBrand);
 		}
 
+        // Criteria => base(x => x.Id == id) 
         public ProductWithTypesAndBrandsSpecification(int id)
 			: base(x => x.Id == id)
         {
